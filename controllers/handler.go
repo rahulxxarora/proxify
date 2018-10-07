8c129a381a5114ceb9138a7c1c329b0ac9a2c1f9
Rahul Arora <rahulxxarora@gmail.com>
2018-10-07 13:08:36 +0530

    Code refactored

--- controllers/handler.go
+++ controllers/handler.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"fmt"
-	"io"
 	"net/http"
 
 	"proxify/httpservice"
@@ -17,26 +16,5 @@ func HandleHTTP(w http.ResponseWriter, r *http.Request) {
 func HandleHTTPS(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("Handling HTTPS request to %s\n", r.RequestURI)
 
-	dest_conn := httpservice.SetConnectionToDestination(r)
-	w.WriteHeader(http.StatusOK)
-
-	hijacker, ok := w.(http.Hijacker)
-	if !ok {
-		panic("Hijacking not supported")
-	}
-
-	client_conn, _, err := hijacker.Hijack()
-	if err != nil {
-		panic(err)
-	}
-
-	go transfer(dest_conn, client_conn)
-	go transfer(client_conn, dest_conn)
-}
-
-func transfer(destination io.WriteCloser, source io.ReadCloser) {
-	defer destination.Close()
-	defer source.Close()
-
-	io.Copy(destination, source)
+	httpservice.MakeProxyHTTPSReqest(w, r)
 }
--- httpService/request.go
+++ httpService/request.go
@@ -55,7 +55,7 @@ func MakeProxyHTTPReqest(w http.ResponseWriter, r *http.Request) {
 	response.Body.Close()
 }
 
-func SetConnectionToDestination(r *http.Request) net.Conn {
+func setConnectionToDestination(r *http.Request) net.Conn {
 	dest_conn, err := net.DialTimeout("tcp", r.Host, 10*time.Second)
 
 	if err != nil {
@@ -63,4 +63,29 @@ func SetConnectionToDestination(r *http.Request) net.Conn {
 	}
 
 	return dest_conn
+}
+
+func MakeProxyHTTPSReqest(w http.ResponseWriter, r *http.Request) {
+	dest_conn := setConnectionToDestination(r)
+	w.WriteHeader(http.StatusOK)
+
+	hijacker, ok := w.(http.Hijacker)
+	if !ok {
+		panic("Hijacking not supported")
+	}
+
+	client_conn, _, err := hijacker.Hijack()
+	if err != nil {
+		panic(err)
+	}
+
+	go transfer(dest_conn, client_conn)
+	go transfer(client_conn, dest_conn)
+}
+
+func transfer(destination io.WriteCloser, source io.ReadCloser) {
+	defer destination.Close()
+	defer source.Close()
+
+	io.Copy(destination, source)
 }
